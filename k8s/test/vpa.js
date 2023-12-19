ff0e1111003bb5862b4e5770611bad7ce2a334a7
quangpham512 <phamquang05122002@gmail.com>
2023-12-19 23:59:48 +0700

    test

---
+++ k8s/test/vpa.js
@@ -0,0 +1,28 @@
+import { sleep } from 'k6';
+import http from 'k6/http';
+import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
+
+export function handleSummary(data) {
+	return {
+		'summary.html': htmlReport(data),
+	};
+}
+// init
+
+export let options = {
+	scenarios: {
+		contacts: {
+			executor: 'ramping-vus',
+			startVUs: 0,
+			stages: [{ duration: '5s', target: 1000 }],
+			gracefulRampDown: '0s',
+		},
+	},
+};
+
+//vus
+export default function () {
+	let res = http.get('http://34.81.200.141/vpa');
+	console.log(res.status);
+}
